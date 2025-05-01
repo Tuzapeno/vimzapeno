ecc447f81cc89d2f5f9ae2a928449620cf267bea
Tuzapeno <tuzis6@gmail.com>
2025-05-01 20:06:54 -0300

    docs(plugins): Add better descriptions to keybindings.lua

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -1,11 +1,17 @@
 local map = vim.keymap.set
 
+-- ================= NAVIGATION =================
+
+-- Quick window switch
 map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
 map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
 map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 
-map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
-
+-- ================== FILES ==================
 map("n", "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
 map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
+
+-- =================== MISC ==================
+
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
