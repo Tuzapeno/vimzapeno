b92e7e06d78fc0e2f0d91adf99dc56170154d5b5
Tuzapeno <tuzis6@gmail.com>
2025-05-01 21:02:19 -0300

    feat(plugins): add cellular-automaton.nvim plugin

---
+++ lua/plugins/cellular-automaton.lua
@@ -0,0 +1,6 @@
+return {
+    -- Seriously cool plugin, run `CellularAutomaton make_it_rain` and see it by yourself
+    "eandrju/cellular-automaton.nvim",
+    lazy = true,
+    cmd = { "CellularAutomaton" },
+}
