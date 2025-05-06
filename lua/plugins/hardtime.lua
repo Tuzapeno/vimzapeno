abf3eda071e78019863318d1ea21e0a38a54ba1c
Tuzapeno <tuzis6@gmail.com>
2025-05-05 22:42:57 -0300

    feat(plugin): Add hardtime plugin

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,9 @@
+return -- lazy.nvim
+{
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {
+        show_mode = false,
+        disabled_filetypes = { "NvimTree" },
+    },
+}
