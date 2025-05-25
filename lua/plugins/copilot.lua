dc5543cf942cc7908d8a6f0e920b2d96e8b4e6a9
Tuzapeno <tuzis6@gmail.com>
2025-05-25 11:39:18 -0300

    feat(plugin): Replace copilot.vim for copilot.lua plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,24 @@
 return {
     -- Inline autocompletions using Github Copilot
-    "github/copilot.vim",
-    lazy = true,
-    event = { "InsertEnter" },
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup({
+            suggestion = {
+                auto_trigger = true,
+                hide_during_completion = false,
+                keymap = {
+                    accept = false,
+                },
+            },
+        })
+        vim.keymap.set("i", "<Tab>", function()
+            if require("copilot.suggestion").is_visible() then
+                require("copilot.suggestion").accept()
+            else
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+            end
+        end, { desc = "Super Tab" })
+    end,
 }
